f5a0aed865d3de533ac9ba46ceaae8e643d9ca2c
Sam <cassarsam2@gmail.com>
2020-10-11 21:03:31 -0700

    Initial commit

---
+++ lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    print('the hangman seeks blood')
+    return 20
+
+twenty_twenty()
